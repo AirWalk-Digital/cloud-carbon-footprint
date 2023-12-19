43f5a410f04fe46f639991ce91b1f6a866572f36
Akhila Ravi <148759409+akhilaravi123@users.noreply.github.com>
2023-12-19 14:26:56 +0000

    prettier update

--- packages/client/src/common/FilterDropdown/filterDropdownStyles.ts
+++ packages/client/src/common/FilterDropdown/filterDropdownStyles.ts
@@ -7,20 +7,20 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   checkbox: {
     marginRight: theme.spacing(1),
-    marginLeft: theme.spacing(-2)
+    marginLeft: theme.spacing(-2),
   },
   inputLabel: {
-    textTransform: "none"
+    textTransform: "none",
   },
   listBox: {
-    overflowWrap: "anywhere" //this works but cuts words out at the end, try to find better solution
+    overflowWrap: "anywhere" //this works but cuts words out at the end, try to find better solution,
   },
   textField: {
     backgroundColor: theme.palette.background.paper,
     overflow: "none",
     borderRadius: theme.shape.borderRadius,
-    height: theme.spacing(5)
-  }
+    height: theme.spacing(5),
+  },
 }));
 
 export default useStyles;
