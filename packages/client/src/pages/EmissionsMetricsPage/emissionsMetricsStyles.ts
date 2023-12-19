8d7b876f25086ad1eecb82a106495d1b138e8ca1
Akhila Ravi <148759409+akhilaravi123@users.noreply.github.com>
2023-12-19 15:35:41 +0000

    prettier update

--- packages/client/src/pages/EmissionsMetricsPage/emissionsMetricsStyles.ts
+++ packages/client/src/pages/EmissionsMetricsPage/emissionsMetricsStyles.ts
@@ -8,27 +8,27 @@ const PADDING_LOADING = 2;
 
 const useStyles = makeStyles((theme) => ({
   boxContainer: {
-    padding: theme.spacing(3, 10)
+    padding: theme.spacing(3, 10),
   },
   loadingContainer: {
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
     justifyItems: "center",
-    minHeight: "100vh"
+    minHeight: "100vh",
   },
   gridCardRow: {
     flexDirection: "row",
-    flexWrap: "wrap-reverse"
+    flexWrap: "wrap-reverse",
   },
   loadingMessage: {
     padding: theme.spacing(PADDING_LOADING),
-    fontSize: "24px"
+    fontSize: "24px",
   },
   pageContainer: {
     height: "100%",
-    overflowY: "scroll"
-  }
+    overflowY: "scroll",
+  },
 }));
 
 export default useStyles;
