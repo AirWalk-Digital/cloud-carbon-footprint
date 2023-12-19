70e9cdd7eb33b3fca30eee9667b53be2d800f898
Akhila Ravi <148759409+akhilaravi123@users.noreply.github.com>
2023-12-19 14:54:22 +0000

    prettier update

--- packages/client/src/pages/EmissionsMetricsPage/CarbonComparisonCard/carbonComparisonStyles.ts
+++ packages/client/src/pages/EmissionsMetricsPage/CarbonComparisonCard/carbonComparisonStyles.ts
@@ -8,60 +8,60 @@ import { CCFTheme } from "../../../utils/themes";
 const useStyles = makeStyles<CCFTheme>(({ palette, spacing }) => {
   return {
     title: {
-      color: palette.primary.contrastText
+      color: palette.primary.contrastText,
     },
     posOne: {
-      color: palette.primary.contrastText
+      color: palette.primary.contrastText,
     },
     posTwo: {
-      maxWidth: 250
+      maxWidth: 250,
     },
     topContainer: {
       backgroundColor: palette.primary.main,
       textAlign: "center",
-      borderRadius: "12px 12px 0 0"
+      borderRadius: "12px 12px 0 0",
     },
     bottomContainer: {
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
       height: "55%",
-      paddingTop: "10%"
+      paddingTop: "10%",
     },
     buttonContainer: {
       display: "flex",
-      justifyContent: "center"
+      justifyContent: "center",
     },
     metricOne: {
       color: palette.primary.contrastText,
-      fontWeight: "bold"
+      fontWeight: "bold",
     },
     metricTwo: {
       color: palette.primary.light,
-      fontWeight: "bold"
+      fontWeight: "bold",
     },
     icon: {
       height: 240,
       width: 200,
-      color: palette.primary.light
+      color: palette.primary.light,
     },
     source: {
       padding: spacing(2),
       display: "flex",
-      justifyContent: "center"
+      justifyContent: "center",
     },
     sourceLink: {
       padding: spacing(0, 1),
       display: "inline-flex",
       alignItems: "center",
-      color: palette.extLink
+      color: palette.extLink,
     },
     openIcon: {
-      marginLeft: "8px"
+      marginLeft: "8px",
     },
     noData: {
-      marginTop: "75px"
-    }
+      marginTop: "75px",
+    },
   };
 });
 
