85125542209a4e684fa8950ec6c35e8afb7641c6
Akhila Ravi <148759409+akhilaravi123@users.noreply.github.com>
2023-12-19 21:43:15 +0000

    prettier update

--- packages/client/src/common/AccountFilter/AccountFilter.tsx
+++ packages/client/src/common/AccountFilter/AccountFilter.tsx
@@ -4,10 +4,11 @@
 
 import React, { FunctionComponent } from "react";
 import FilterDropdown from "../../common/FilterDropdown";
-import { DropdownFilterOptions,
-         DropdownOption,
-         FilterProps, 
-       } from "../../Types";
+import { 
+   DropdownFilterOptions,
+   DropdownOption,
+   FilterProps, 
+} from "../../Types";
 
 const AccountFilter: FunctionComponent<FilterProps> = ({
   filters,
