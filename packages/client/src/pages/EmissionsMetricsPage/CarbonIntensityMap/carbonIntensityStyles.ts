676f00a2052178f0fed1c9f7f4beae83864650e2
Akhila Ravi <148759409+akhilaravi123@users.noreply.github.com>
2023-12-19 15:06:16 +0000

    prettier update

--- packages/client/src/pages/EmissionsMetricsPage/CarbonIntensityMap/carbonIntensityStyles.ts
+++ packages/client/src/pages/EmissionsMetricsPage/CarbonIntensityMap/carbonIntensityStyles.ts
@@ -12,19 +12,19 @@ const useStyles = makeStyles(() => ({
     opacity: "1",
     fontWeight: 900,
     color: "rgba(0, 0, 0, 0.87)",
-    padding: ".2em"
+    padding: ".2em",
   },
   topContainer: {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
     width: "100%",
-    marginBottom: "24px"
+    marginBottom: "24px",
   },
   map: {
     height: "100%",
-    width: "100%"
-  }
+    width: "100%",
+  },
 }));
 
 export default useStyles;
