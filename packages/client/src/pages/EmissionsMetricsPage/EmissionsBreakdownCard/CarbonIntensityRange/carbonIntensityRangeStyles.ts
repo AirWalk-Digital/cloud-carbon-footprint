f549d31e601b1b27e8b3c55c58b18b78a9694ed8
Akhila Ravi <148759409+akhilaravi123@users.noreply.github.com>
2023-12-19 15:12:09 +0000

    prettier update

--- packages/client/src/pages/EmissionsMetricsPage/EmissionsBreakdownCard/CarbonIntensityRange/carbonIntensityRangeStyles.ts
+++ packages/client/src/pages/EmissionsMetricsPage/EmissionsBreakdownCard/CarbonIntensityRange/carbonIntensityRangeStyles.ts
@@ -15,30 +15,30 @@ const useStyles = ({ palette }: Theme): ReactStyleProperties => ({
     flexDirection: "column",
     width: "100%",
     padding: "10px 45px",
-    alignSelf: "center"
+    alignSelf: "center",
   },
   labelsStyle: {
     display: "flex",
     justifyContent: "space-between",
     paddingBottom: "12px",
-    fontWeight: 500
+    fontWeight: 500,
   },
   timelineStyles: {
     display: "flex",
     width: "100%",
     justifyContent: "space-between",
-    alignItems: "center"
+    alignItems: "center",
   },
   dotStyles: {
     width: "15px",
     height: "15px",
-    borderRadius: "8px"
+    borderRadius: "8px",
   },
   barStyles: {
     flexGrow: 2,
     height: "1px",
-    backgroundColor: palette.grey.A200
-  }
+    backgroundColor: palette.grey.A200,
+  },
 });
 
 export default useStyles;
