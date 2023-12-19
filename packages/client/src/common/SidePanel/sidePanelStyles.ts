7183feb12bf33b7f631c2303b432fd1c01476dda
Akhila Ravi <148759409+akhilaravi123@users.noreply.github.com>
2023-12-19 14:36:38 +0000

    prettier update

--- packages/client/src/common/SidePanel/sidePanelStyles.ts
+++ packages/client/src/common/SidePanel/sidePanelStyles.ts
@@ -10,48 +10,48 @@ const useStyles = makeStyles(
     infoButton: {
       margin: spacing(10, "auto"),
       color:
-        palette.type === "dark" ? palette.text.primary : palette.primary.main
+        palette.type === "dark" ? palette.text.primary : palette.primary.main,
     },
     closeButtonContainer: {
-      textAlign: "right"
+      textAlign: "right",
     },
     drawer: {
       width: ({ drawerWidth }: SidePanelProps) => drawerWidth,
-      flexShrink: 0
+      flexShrink: 0,
     },
     drawerOpen: {
       width: ({ drawerWidth }: SidePanelProps) => drawerWidth,
       transition: transitions.create("width", {
         easing: transitions.easing.sharp,
-        duration: transitions.duration.enteringScreen
-      })
+        duration: transitions.duration.enteringScreen,
+      }),
     },
     drawerClose: {
       transition: transitions.create("width", {
         easing: transitions.easing.sharp,
-        duration: transitions.duration.leavingScreen
+        duration: transitions.duration.leavingScreen,
       }),
       overflowX: "hidden",
       width: 0, //spacing(4) + 1,
       [breakpoints.up("sm")]: {
-        width: spacing(6) + 1
+        width: spacing(6) + 1,
       },
-      marginRight: 14 // Offset to make scrollbar visible
+      marginRight: 14, // Offset to make scrollbar visible
     },
     toolbar: {
       padding: spacing(10, 1),
       // necessary for content to be below app bar
-      ...mixins.toolbar
+      ...mixins.toolbar,
     },
     hide: {
-      display: "none"
+      display: "none",
     },
     contentTitle: {
       padding: spacing(2),
       textAlign: "center",
       fontWeight: "bold",
-      fontSize: 18
-    }
+      fontSize: 18,
+    },
   })
 );
 
