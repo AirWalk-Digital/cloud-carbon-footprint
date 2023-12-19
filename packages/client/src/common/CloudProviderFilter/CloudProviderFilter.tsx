d8dbf8603773928ee8522c0098f942ab48c9b58c
Akhila Ravi <148759409+akhilaravi123@users.noreply.github.com>
2023-12-19 18:35:03 +0000

    prettier update

--- packages/client/src/common/CloudProviderFilter/CloudProviderFilter.tsx
+++ packages/client/src/common/CloudProviderFilter/CloudProviderFilter.tsx
@@ -5,7 +5,10 @@
 import React, { FunctionComponent } from "react";
 import { CLOUD_PROVIDER_OPTIONS } from "../../common/FilterBar/utils/DropdownConstants";
 import FilterDropdown from "../../common/FilterDropdown";
-import { DropdownFilterOptions, DropdownOption, FilterProps, } from "../../Types";
+import { DropdownFilterOptions,
+         DropdownOption,
+        FilterProps, 
+       } from "../../Types";
 
 const CloudProviderFilter: FunctionComponent<FilterProps> = ({
   filters,
