76c7713ee99e894f0e4028195fd2528aea78ac87
Akhila Ravi <148759409+akhilaravi123@users.noreply.github.com>
2023-12-19 14:38:59 +0000

    prettier update

--- packages/client/src/common/Toggle/toggleStyles.ts
+++ packages/client/src/common/Toggle/toggleStyles.ts
@@ -15,29 +15,29 @@ const useStyles = makeStyles<CCFTheme>(({ palette }) => ({
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center"
+    alignItems: "center",
   },
   label: {
     fontWeight: "bold",
-    textAlign: "center"
+    textAlign: "center",
   },
   toggleInput: {
     position: "absolute",
     left: "-99em",
     "&:checked + $toggle": {
-      background: palette.lightBlue
+      background: palette.lightBlue,
     },
     "&:checked + $toggle:before": {
-      color: palette.primaryBlue
+      color: palette.primaryBlue,
     },
     "&:checked + $toggle:after": {
-      color: "white"
+      color: "white",
     },
     "&:checked + $toggle $toggleHandler": {
       width: 110,
       transform: "translateX(110px)",
-      borderColor: "#fff"
-    }
+      borderColor: "#fff",
+    },
   },
   toggle: {
     cursor: "pointer",
@@ -56,7 +56,7 @@ const useStyles = makeStyles<CCFTheme>(({ palette }) => ({
       zIndex: 2,
       transition: "all 200ms cubic-bezier(0.445, 0.05, 0.55, 0.95)",
       right: 20,
-      top: 15
+      top: 15,
     },
     "&::before": {
       content: '"metric tons"',
@@ -66,8 +66,8 @@ const useStyles = makeStyles<CCFTheme>(({ palette }) => ({
       zIndex: 2,
       transition: "all 200ms cubic-bezier(0.445, 0.05, 0.55, 0.95)",
       top: 15,
-      left: 20
-    }
+      left: 20,
+    },
   },
   toggleHandler: {
     display: "inline-block",
@@ -78,8 +78,8 @@ const useStyles = makeStyles<CCFTheme>(({ palette }) => ({
     height: 50,
     borderRadius: 15,
     transition: "all 200ms cubic-bezier(0.445, 0.05, 0.55, 0.95)",
-    transform: "translateX(0px)"
-  }
+    transform: "translateX(0px)",
+  },
 }));
 
 export default useStyles;
