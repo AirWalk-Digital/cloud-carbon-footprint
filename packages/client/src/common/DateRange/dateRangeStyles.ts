61a9c86fdfbdf127447b4ca1db7d20fdc29a6b29
Akhila Ravi <148759409+akhilaravi123@users.noreply.github.com>
2023-12-19 14:14:44 +0000

    prettier update

--- packages/client/src/common/DateRange/dateRangeStyles.ts
+++ packages/client/src/common/DateRange/dateRangeStyles.ts
@@ -6,8 +6,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
   dateRangeText: {
-    color: "#979797"
-  }
+    color: "#979797",
+  },
 }));
 
 export default useStyles;
