1e924896103d5271d16ea109506d66ea68f3878c
Akhila Ravi <148759409+akhilaravi123@users.noreply.github.com>
2023-12-19 15:18:52 +0000

    prettier update

--- packages/client/src/pages/EmissionsMetricsPage/EmissionsFilterBar/Filters/MonthFilter/monthFilterStyles.ts
+++ packages/client/src/pages/EmissionsMetricsPage/EmissionsFilterBar/Filters/MonthFilter/monthFilterStyles.ts
@@ -7,11 +7,11 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   buttonGroup: {
     height: theme.spacing(5),
-    backgroundColor: theme.palette.background.paper
+    backgroundColor: theme.palette.background.paper,
   },
   button: {
-    transition: "color 1s"
-  }
+    transition: "color 1s",
+  },
 }));
 
 export default useStyles;
